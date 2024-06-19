970873d149687fd0bbd24f11991f8a2a4fe3a8d3
Vedant0370 <vedant.0370@gmail.com>
2024-06-19 12:18:01 +0530

    update index.js

--- index.js
+++ index.js
@@ -27,7 +27,9 @@ const ExecutiveRoutes = require("./routes/ExecutiveRoutes");
 //declearation 
 const app = express();
 app.use(cors());
-app.use(express.json());
+// app.use(express.json());
+app.use(express.json({ limit: '50mb' })); // Increase limit to 50mb
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
 const apiRouter = express.Router();
 const PORT = process.env.PORT;
 
